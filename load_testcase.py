77ae88c8e531d70dce0d53ccbf68f1cb0b58c051
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2020-12-01 23:52:35 +0900

    load_testcase.py

--- load_testcase.py
+++ load_testcase.py
@@ -5,8 +5,10 @@ from login import login
 
 def main(args):
     contest = args[1]
-    problems = 'abcdef' if args[2] == '-' else args[2]
-    session = login() if args[3] == 'y' else requests.session()
+    problems = 'ABCDEF' if args[2] == '-' else args[2]
+    session = requests.session()
+    if args[3] == 'y':
+        login(session)
 
     url = 'https://atcoder.jp/contests/{0}/tasks/{0}_'.format(contest)
 
@@ -14,33 +16,32 @@ def main(args):
         load_problem(session, url, problem)
 
 def load_problem(session, url, problem):
-    path = '../test/' + problem
+    test_home = '../test/' + problem
 
     parts = get_soup(session, url + problem.lower()).select('.part')
+    num = extract_and_output(parts, '入力例', test_home + '/in')
+    extract_and_output(parts, '出力例', test_home + '/out')
 
-    count = extract_and_output(parts, '入力例', path + '/in')
-    extract_and_output(parts, '出力例', path + '/out')
-
-    make_list(count, path)
+    make_list(num, test_home)
 
 def get_soup(session, url):
-    return BeautifulSoup(session.get(url).text, 'html.parser')
+    return BeautifulSoup(session.get(url).text, 'lxml')
 
 def extract_and_output(result_set, title, path):
     sample_iter = extract_sample(result_set, title)
-    count = output_sample(sample_iter, path)
-    return count
+    num = output_sample(sample_iter, path)
+    return num
 
 def extract_sample(result_set, title):
     data = filter(lambda e : title == e.select_one('h3').text[:len(title)], result_set)
     return map(lambda e : e.select_one('pre').text.replace('\r', ''), data)
 
 def output_sample(sample_iter, path):
-    for i, sample in enumerate(sample_iter):
-        file = '{}/{}.txt'.format(path, i + 1)
+    for i, sample in enumerate(sample_iter, 1):
+        file = '{}/{}.txt'.format(path, i)
         with open(file, 'w', newline = '\n') as f:
             f.write(sample)
-    return i + 1
+    return i
 
 def make_list(count, path):
     sample_list = list(str(i) + '.txt' for i in range(1, count + 1))
