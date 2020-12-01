a58a3145bf9e91f86947ff27a6888447fc317155
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2020-12-02 00:01:02 +0900

    sub and rank

--- rank.bat
+++ rank.bat
@@ -1,2 +1,2 @@
 @echo off
-start https://atcoder.jp/contests/abc%1/submissions?f.LanguageName=Java^&f.Status=AC^&f.Task=abc%1_%2^&f.User=^&orderBy=time_consumption
+start https://atcoder.jp/contests/%1/submissions?f.LanguageName=Java^&f.Status=AC^&f.Task=%1_%2^&f.User=^&orderBy=time_consumption
--- sub.bat
+++ sub.bat
@@ -1,2 +1,2 @@
 @echo off
-start https://atcoder.jp/contests/abc0%1/submissions/me
+start https://atcoder.jp/contests/%1/submissions?f.Task=^&f.LanguageName=Java^&f.Status=AC^&f.User=yama2019
