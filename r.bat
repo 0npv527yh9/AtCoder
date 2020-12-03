fc391319faf5f6ee36f168ebc45aab176a36e971
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2020-12-03 20:13:20 +0900

    create r.bat

---
+++ r.bat
@@ -0,0 +1,2 @@
+@echo off
+start https://atcoder.jp/contests/%contest%/submissions?f.LanguageName=Java^&f.Status=AC^&f.Task=%contest%_%1^&f.User=^&orderBy=time_consumption
--- rank.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start https://atcoder.jp/contests/%1/submissions?f.LanguageName=Java^&f.Status=AC^&f.Task=%1_%2^&f.User=^&orderBy=time_consumption
