987a5e0bbd4e5dc2139a2d853edf35731be2ad44
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-02-14 11:13:28 +0900

    refact atcoder.py

--- atcoder.py
+++ atcoder.py
@@ -1,20 +1,17 @@
 import sys
+
 import compile
 import test
 import submit
 
 def main(args):
-    contest = args[1]
-    problem = args[2]
-    language = args[3]
-    sub = args[4]
+    problem, language, sub = args[1:]
 
     compile.compile(language)
-
     test.test(problem, language)
 
     if sub == '':
-        submit.submit(contest, problem, language)
+        submit.submit(problem, language)
 
 if __name__ == '__main__':
     main(sys.argv)
