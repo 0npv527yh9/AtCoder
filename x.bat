05d216e182defd3cd05310161366e801448831f0
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-03-19 23:41:18 +0900

    [change] x.bat
    >> x
    でtestフォルダのフルパスをコピー

--- x.bat
+++ x.bat
@@ -1,7 +1,11 @@
 @echo off
-cd ..\test\%1
-del * /S /Q > nul
-7z x ..\%1.zip
-del ..\%1.zip
-call ac
-call m %1
+if "%1" == "" (
+  echo D:\Software\atcoder\test | clip
+) else (
+  cd ..\test\%1
+  del * /S /Q > nul
+  7z x ..\%1.zip
+  del ..\%1.zip
+  call ac
+  call m %1
+)
