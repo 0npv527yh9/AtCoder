c8cc9047e29b7657b3eef8acb9b5168e91b79286
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-03-28 17:21:10 +0900

    [change] atcoder.py
    - コンパイル・テストなしの強制提出
    ex)
    >> j a f

--- atcoder.py
+++ atcoder.py
@@ -7,10 +7,11 @@ import submit
 def main(args):
     problem, language, sub = args[1:]
 
-    compile.compile(language)
-    test.test(problem, language)
+    if sub != 'f':
+        compile.compile(language)
+        test.test(problem, language)
 
-    if sub == '':
+    if sub == '' or sub == 'f':
         submit.submit(problem, language)
 
 if __name__ == '__main__':
