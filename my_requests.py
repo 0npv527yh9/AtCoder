1a4440c52d576c9888c25c59c7179a9f525cb66d
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-04-08 20:25:43 +0900

    [add] my_requests.py
    - テンプレート

---
+++ my_requests.py
@@ -0,0 +1,11 @@
+from requests import Session
+
+class MySession(Session):
+    def __init__(self):
+        pass
+
+    def get(self, url):
+        pass
+
+    def post(self, url, data):
+        pass
