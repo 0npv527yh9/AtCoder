b7590f12dba7dee52f63eaa5afb79a702b78dac6
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-04-08 20:17:06 +0900

    [remake] load_testcase.py
    - 大幅な変更
    >> l <contest top page> // 全ての問題のテストケースを保存
    >> l <problem page> // その問題のテストケースを保存

--- load_testcase.py
+++ load_testcase.py
@@ -1,83 +1,113 @@
 import sys
-from time import sleep
+from time import sleep, time
 from os.path import exists
 from os import mkdir
 from shutil import rmtree
+import re
 
-import requests
 from bs4 import BeautifulSoup
 
-import login
-from config import register_contest
+from login import login
+from config import register_contest, session
+
+timestamp = 0
+INTERVAL = 2
 
 def main(args):
-    title = args[1]
-    problems = args[2]
-    session = requests.session()
-
-    needs_login = False
-    prefix = title.replace('-', '_')
-
-    i = 3
-    while i < len(args):
-        if args[i] == '-l':
-            needs_login = True
-        elif args[i] == '-p':
-            i += 1
-            prefix = args[i]
-        i += 1
-
-    if needs_login:
-        login.login(session)
-
-    url = f'https://atcoder.jp/contests/{title}/tasks/{prefix}_'
-
-    for problem in problems.upper():
-        load_problem(session, url, problem)
-        sleep(2)
-
+    url = args[1]
+    url, title, prefix = extract_contest_data(session, url)
     register_contest(title, prefix)
 
-def load_problem(session, url, problem):
-    test_home = '../test/' + problem
-    if exists(test_home):
-        rmtree(test_home)
-    mkdir(test_home)
+    tasks = load_tasks(session, url)
+    save_tasks(tasks)
 
-    parts = create_soup(session, url + problem.lower()).select('.part')
-    num = extract_and_output(parts, '入力例', test_home + '/in')
-    extract_and_output(parts, '出力例', test_home + '/out')
+def extract_contest_data(session, url):
+    top_page_pattern = 'https://atcoder.jp/contests/([^/]+)'
+    task_page_pattern = f'{top_page_pattern}/tasks/(.+)_.+'
+    if (m := re.fullmatch(top_page_pattern, url)):
+        title = m.groups()[0]
+        # login(session)
+        prefix = load_prefix(session, f'{url}/tasks')
+        url = f'{url}/tasks_print'
+    elif (m := re.fullmatch(task_page_pattern, url)):
+        title, prefix = m.groups()
+    else:
+        print('URL must meet either of the following styles:',
+              f'* {top_page_pattern}',
+              f'* {task_page_pattern}',
+              sep = '\n')
+        exit(1)
+    return url, title, prefix
 
-    make_list(num, test_home)
-    print(problem, num)
+def load_prefix(session, url):
+    bs = create_soup(session, url)
+    url = bs.find('h2').parent.a.get('href')
+    prefix = re.findall('([^/]+)_', url)[0]
+    return prefix
+
+def load_tasks(session, url):
+    bs = create_soup(session, url)
+    tasks = extract_tasks(bs)
+    return tasks
 
 def create_soup(session, url):
-    try:
-        res = session.get(url)
-        res.raise_for_status()
-        return BeautifulSoup(res.text, 'lxml')
-    except Exception as e:
-        print(e)
-        sys.exit(res.status_code)
+    stamp()
+    res = session.get(url)
+    res.raise_for_status()
+    return BeautifulSoup(res.text, 'lxml')
 
-def extract_and_output(result_set, title, path):
-    sample_iter = extract_sample(result_set, title)
-    num = output_sample(sample_iter, path)
-    return num
+def stamp():
+    global timestamp
+    t = time()
+    wait = max(0, INTERVAL - (t - timestamp))
+    sleep(wait)
+    timestamp = t
 
-def extract_sample(result_set, title):
-    match_title = lambda e : title == e.select_one('h3').text[:len(title)]
-    data = filter(match_title, result_set)
-    extract_sample = lambda e : e.select_one('pre').text.lstrip().replace('\r', '')
-    return map(extract_sample, data)
+def save_tasks(tasks):
+    loaded = []
+    failed = []
+    for task in tasks:
+        try:
+            title, samples = extract_samples(task)
+            save_samples(title, samples)
+            loaded.append(title)
+        except:
+            failed.append(title)
+    print('loaded:', *loaded)
+    print('failed:', *failed)
 
-def output_sample(sample_iter, path):
-    mkdir(path)
-    for i, sample in enumerate(sample_iter, 1):
-        file = f'{path}/{i}.txt'
-        with open(file, 'w', newline = '\n') as f:
-            f.write(sample)
-    return i
+def extract_tasks(bs):
+    titles = bs.find_all('span', class_ = 'h2')
+    tasks = tuple(map(lambda title : title.parent, titles))
+    return tasks
+
+def extract_samples(task):
+    title = task.span.text.strip()[0]
+    samples = task.find_all(string = re.compile('(入|出)力例 *[0-9]'))
+    samples = map(extract_sample, samples)
+    samples = tuple(samples)
+    samples = tuple(zip(samples[::2], samples[1::2]))
+    return title, samples
+
+def extract_sample(tag):
+    s = tag.parent.find_next_sibling('pre').text
+    return s.strip().replace('\r', '') + '\n'
+
+def save_samples(title, samples):
+    root = f'../test/{title}'
+    if exists(root):
+        rmtree(root)
+    mkdir(root)
+    mkdir(f'{root}/in')
+    mkdir(f'{root}/out')
+    for i, (in_, out) in enumerate(samples, 1):
+        write(f'{root}/in/{i}.txt', in_)
+        write(f'{root}/out/{i}.txt', out)
+    make_list(i, root)
+
+def write(file, s):
+    with open(file, 'w', newline = '\n') as f:
+        f.write(s)
 
 def make_list(count, path):
     sample_list = list(f'{i}.txt' for i in range(1, count + 1))
