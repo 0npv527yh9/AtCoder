4a2be88be2cd28356486f28726852479b3d21e4f
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-04-08 19:54:50 +0900

    [remake] login.py
    - loginしてcookiesをpickleでdump

--- login.py
+++ login.py
@@ -1,21 +1,36 @@
+from time import sleep
+from pickle import dump
+
+import requests
 from bs4 import BeautifulSoup
+
 from config import login_data
 
-url = 'https://atcoder.jp/login'
+INTERVAL = 10
 
-def login(session):
-    # get csrf_token
-    print('Login...', end = ' ')
+def main():
+    url = 'https://atcoder.jp/login'
+    session = requests.session()
+    csrf_token = load_csrf_token(session, url)
+    sleep(INTERVAL)
+    login(session, url, csrf_token)
+    save('cookies.pickle', session.cookies)
+
+def load_csrf_token(session, url):
     res = session.get(url)
     bs = BeautifulSoup(res.text, 'lxml')
-    csrf_token = bs.select_one("[name = 'csrf_token']").get('value')
-    login_data['csrf_token'] = csrf_token
-
-    # post login data
-    res = session.post(url, login_data)
-
-    # check response
-    message = 'OK!' if res.status_code == 200 else 'Failed.'
-    print(message)
-
+    csrf_token = bs.find(attrs = {'name': 'csrf_token'}).get('value')
     return csrf_token
+
+def login(session, url, csrf_token):
+    login_data['csrf_token'] = csrf_token
+    res = session.post(url, data = login_data)
+    res.raise_for_status()
+    print(res.reason)
+
+def save(file, obj):
+    with open(file, 'wb') as f:
+        dump(obj, f)
+
+if __name__ == '__main__':
+    main()
