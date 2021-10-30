d0538202aa5414f0b44e86f37d6fdd1a53f22cc4
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-10-30 10:03:46 +0900

    fix g.bat

--- g.bat
+++ g.bat
@@ -1,2 +1,7 @@
 @echo off
-python atcoder.py %contest% %1 c++ "%2"
+
+if "%1" == "" (
+  call g++ ..\src\main.cpp -o ..\build\cpp.exe -I ..\alc\ac-library && ..\build\cpp.exe < ..\src\in.txt
+) else (
+  python atcoder.py %contest% %1 c++ "%2"
+)
