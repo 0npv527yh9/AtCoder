c9d0c9b71935654728a426f1f71d13a7bed30b1a
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-10-30 10:08:31 +0900

    fix r.bat

--- r.bat
+++ r.bat
@@ -1,2 +1,7 @@
 @echo off
-python atcoder.py %1 rust "%2"
+
+if "%1" == "" (
+  rustc ..\src\rust\src\main.rs -o ..\build\rust.exe && ..\build\rust.exe < ..\src\in.txt
+) else (
+    python atcoder.py %1 rust "%2"
+)
