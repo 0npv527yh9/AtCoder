2a2926394f727646f9bd11a581bd60f7fb042ce6
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-11-08 00:29:00 +0900

    change CRLF to LR

--- ans.py
+++ ans.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from my_requests import AtCoderSession
 from contest import title, prefix
@@ -19,11 +20,11 @@ def main(args):
     submission_url = url = top_url + session.soup.tbody.tr.find_all('td')[-1].a['href']
     session.get(submission_url)
 
-    code = session.soup.find('pre', id = 'submission-code').text
+    code = session.soup.find('pre', id = 'submission-code').text.replace('\r', '')
     write('../src/main.' + extension, code)
 
 def write(file, s):
-    with open(file, 'w', encoding = 'utf-8') as f:
+    with open(file, 'w', encoding = 'utf-8', newline = '\n') as f:
         f.write(s)
 
 if __name__ == '__main__':
