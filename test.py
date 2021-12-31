3eaca54ed99f08ee8fac3e220ae3344456406843
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-12-31 10:37:15 +0900

    modify test.py

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
+import os
 import re
-import sys
 import shutil
 import subprocess
+import sys
+
 from config import language_dict
 
 col, row = shutil.get_terminal_size()
 
-def main(args):
-    problem = args[1].upper()
-    language = args[2].lower()
+
+def main():
+    problem = sys.argv[1].upper()
+    language = sys.argv[2].lower()
     test(problem, language)
+
 
 def test(problem, language):
     test_home = '../test/' + problem
 
-    sample_file_list = read(test_home + '/list.txt').split('\n')
+    sample_file_list = sorted(os.listdir(test_home + '/in'))
 
     AC = True
 
@@ -22,36 +26,38 @@ def test(problem, language):
         input_file = test_home + '/in/' + sample_file
         output_file = test_home + '/out/' + sample_file
 
-        actual = execute(language, input_file)
+        output, error = execute(language, input_file)
         expected = read(output_file)
 
-        if expected != actual:
+        if expected != output:
             input_ = read(input_file)
-            print_diff(sample_file, trim(input_), trim(expected), trim(actual))
+            print_diff(sample_file, trim(input_), trim(expected),
+                       f'{trim(output)}\n\n{error}')
             AC = False
-    
+            if error:
+                break
+
     if AC:
         print('AC')
 
     return AC
 
+
 def read(file):
     with open(file) as f:
         return f.read().strip()
 
+
 def execute(language, input_file):
     command = language_dict[language]['execute']
-    option = {
-        'stdin': open(input_file),
-        'capture_output': True
-    }
+    option = {'stdin': open(input_file), 'capture_output': True}
     res = subprocess.run(command, **option)
-    if res.returncode:
-        res = res.stdout.decode() + '\n' + res.stderr.decode()
-    else:
-        res = res.stdout.decode()
-        res = re.sub(' *\r?\n', '\n', res)
-    return res.strip()
+    out = res.stdout.decode()
+    out = re.sub(' *\r?\n', '\n', out).strip()
+    err = res.stderr.decode().strip()
+
+    return out, err
+
 
 def print_diff(file, input_, expected, actual):
     title_tuple = ('input', 'expected', 'actual')
@@ -63,12 +69,10 @@ def print_diff(file, input_, expected, actual):
         print(content)
     print('\n')
 
+
 def print_title(title, style = '-'):
-    w = max(0, col - len(title))
-    l = w // 2
-    r = w - l
-    s = style * l + title + style * r
-    print(s)
+    print(title.center(col, style))
+
 
 def trim(s):
     l = s.split('\n')
@@ -76,9 +80,11 @@ def trim(s):
     l = l[:h] + ['...'] + l[-h:] if row < len(l) else l
     lw = col - 3 >> 1
     rw = col - 3 - lw
-    l = list(map(lambda s : s if col >= len(s) else s[:lw] + '...' + s[-rw:], l))
+    l = list(map(lambda s: s
+                 if col >= len(s) else s[:lw] + '...' + s[-rw:], l))
     s = '\n'.join(l)
     return s
 
+
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
