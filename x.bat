9163f985d80884d9dc4240407495f82f2e7aa908
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-12-31 12:18:27 +0900

    modify x.bat

--- x.bat
+++ x.bat
@@ -1,11 +1,17 @@
 @echo off
 if "%1" == "" (
-  echo D:\Software\atcoder\test | clip
-) else (
+  echo D:\Software\atcoder\temp | clip
+) else if "%2" == "" (
   cd ..\test\%1
   del * /S /Q > nul
-  7z x ..\%1.zip
-  del ..\%1.zip
+  7z x ..\..\temp\%1.zip
+  del ..\..\temp\%1.zip
   call ac
-  call m %1
+) else (
+  cd ..
+  rd /S /Q test
+  md test
+  cd test
+  7z x ..\temp\%1.zip
+  call ac
 )
