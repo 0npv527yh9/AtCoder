9892d316ae780cd6faecc4a79a5873687aa9b6a0
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2021-12-31 11:54:23 +0900

    implement option dict

--- atcoder.py
+++ atcoder.py
@@ -1,19 +1,46 @@
 import sys
+import test
 
 import compile
-import test
 import submit
+from config import option_dict
+from load_testcase import save_contest_data
 
-def main(args):
-    problem, language, sub = args[1:]
 
-    if sub != 'f':
+def main():
+    language, task = sys.argv[1:3]
+    option_dict.update(language = language, task = task)
+    load_option_dict(option_dict, sys.argv[3:])
+
+    if option_dict['test']:
         compile.compile(language)
-        AC = test.test(problem, language)
-        sub = sub == '' and AC
+        AC = test.test(language, task)
+        option_dict['submit'] &= AC
 
-    if sub:
-        submit.submit(problem, language)
+    if option_dict['submit']:
+        submit.submit(option_dict)
+
+
+def load_option_dict(option_dict, args):
+    args = iter(args)
+    while True:
+        try:
+            mode = next(args)
+        except:
+            break
+
+        if mode == 't':
+            option_dict['task'] = next(args)
+        elif mode == 'p':
+            option_dict['prefix'] = next(args)
+            save_contest_data(title, option_dict['prefix'])
+        elif mode == 'f':
+            option_dict['test'] = False
+        elif mode == 'n':
+            option_dict['submit'] = False
+        else:
+            raise Exception('Invalid option: ' + mode)
+
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
