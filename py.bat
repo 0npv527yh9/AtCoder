d70e68d531e7017a16b9c812f08f560afbca17ea
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2022-04-12 14:57:46 +0900

    support pypy

--- py.bat
+++ py.bat
@@ -1,9 +1,7 @@
 @echo off
 
 if "%1" == "" (
-  call python ..\src\main.py < ..\src\in.txt
+  python ..\src\main.py < ..\src\in.txt
 ) else (
-  call pypy
   python atcoder.py pypy %1 %2 %3 %4 %5 %6 %7 %8 %9
-  call ac
 )
