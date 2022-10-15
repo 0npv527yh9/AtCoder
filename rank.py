7e7e300a02587e4ed4b0d230dcbfacde5c0cd9d6
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2022-10-15 15:23:41 +0900

    fix rank.py

--- rank.py
+++ rank.py
@@ -1,7 +1,9 @@
 import sys
-from config import language_dict
-from contest import title, prefix
 import webbrowser
+
+from config import language_dict
+from contest import prefix, title
+
 
 def main(args):
     problem = args[1].lower()
@@ -10,6 +12,7 @@ def main(args):
 
     url = f'https://atcoder.jp/contests/{title}/submissions?f.Language={languageID}&f.Status=AC&f.Task={prefix}_{problem}&orderBy=time_consumption'
     webbrowser.open(url)
+
 
 if __name__ == '__main__':
     main(sys.argv)
