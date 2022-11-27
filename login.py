c6cae675b72b1bc0e51132f27c0e842281532a3f
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2022-11-28 02:30:41 +0900

    fix login

--- login.py
+++
@@ -1,7 +0,0 @@
-from my_requests import AtCoderSession
-
-def main():
-    session = AtCoderSession(login = True)
-
-if __name__ == '__main__':
-    main()
--- my_requests.py
+++ my_requests.py
@@ -4,32 +4,36 @@ import time
 from bs4 import BeautifulSoup
 from requests import Session
 
-from private import login_data
-
 
 class AtCoderSession(Session):
-    __INTERVAL = 10
+    __INTERVAL = 1
     __COOKIES_PICKLE = 'cookies.pickle'
     __LOGIN_URL = 'https://atcoder.jp/login'
 
-    def __init__(self, login = False):
+    def __init__(self):
         super().__init__()
         self.__timestamp = 0
-        self.soup = None
-        if login:
-            self.__login()
-        else:
+        self.soup: BeautifulSoup
+        try:
             self.__load()
+        except:
+            self.__login()
 
     def __del__(self):
-        self.__save()
-        print('Session was saved.')
+        try:
+            self.__save()
+        except:
+            pass
 
     def __login(self):
+        login_data = _import_login_data()
+
         self.get(AtCoderSession.__LOGIN_URL)
         self.csrf_token = self.__find_csrf_token()
         login_data['csrf_token'] = self.csrf_token
-        self.post(AtCoderSession.__LOGIN_URL, login_data)
+
+        res = self.post(AtCoderSession.__LOGIN_URL, login_data)
+        _exit_if_login_failed(res)
 
     def __find_csrf_token(self):
         return self.soup.find(attrs = {'name': 'csrf_token'}).get('value')
@@ -58,7 +62,24 @@ class AtCoderSession(Session):
         time.sleep(t)
         self.__timestamp = now
 
-    def post(self, url, data):
-        res = super().post(url, data)
-        res.raise_for_status()
-        print(res.reason)
+
+def _import_login_data():
+    try:
+        import private
+        return private.login_data
+    except:
+        print('Create "private.py" in the current directory.')
+        print(
+            'Write "login_data = {\'username\': \'<user name>\', \'password\': \'<password>\'}" in the file.'
+        )
+        print('Save it.')
+        exit(0)
+
+
+def _exit_if_login_failed(res):
+    if BeautifulSoup(res.text, 'lxml').title.text != 'AtCoder':
+        print('Sign In failed.')
+        print(
+            'Please check if "username" and "password" written in "private.py" are correct.'
+        )
+        exit(0)
